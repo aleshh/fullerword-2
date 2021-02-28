1df90d299ef93c7d7f8fc957c9081862f2d5266f
Alesh Houdek <mail@alesh.com>
2021-02-28 05:23:04 -0700

    define preliminary models

--- dictionary/models.py
+++ dictionary/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    user_name = models.CharField(max_length=60)
+    email = models.CharField(max_length=60)
+    # password
+    use_source = models.BooleanField(default=True)
+    use_favorites = models.BooleanField(default=True)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=60)
+
+class Entry(models.Model):
+    word = models.CharField(max_length=120)
+    definition = models.TextField(max_length=1500)
+    source = models.TextField(max_length=5000)
+    favorite = models.BooleanField(default=False)
+    private = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
