bd9156d36b77b393f0828baf971957fb991858be
Alesh Houdek <mail@alesh.com>
2021-02-28 11:52:23 -0700

    add entry view

--- dictionary/urls.py
+++ dictionary/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('test/', views.test, name="test"),
   path('', views.index, name='index'),
+  path('<int:id>', views.entry, name='entry'),
 ]
--- dictionary/views.py
+++ dictionary/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
+from .models import Entry, Tag
 
 def index(request):
   return HttpResponse("Hello world")
 
-def test(request):
-  return HttpResponse("TESTTTTTT")
+def entry(request, id):
+  entry = Entry.objects.get(id=id)
+  return HttpResponse('<h1>%s</h1>' % entry.word)
